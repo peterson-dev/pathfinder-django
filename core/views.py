6c83c650b76d47a276b0dbe535d5538d1653720c
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-10 20:52:35 -0400

    add starter logic for displaying pathfinder

--- core/views.py
+++ core/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from core.forms import PathfinderForm
+from core.pathfinder import ElevationMap, PathFinder, MapImage
+from io import BytesIO
+import base64
 # Create your views here.
+
+
+def index_view(request):
+    form = PathfinderForm()
+    image_data_url = None
+
+    if request.method == "POST":
+        form = PathfinderForm(request.POST, request.FILES)
+        if form.is_valid():
+            elevation_file = request.FILES['elevation_file']
+            data = elevation_file.read().decode('utf-8').strip().split("\n")
+            map = ElevationMap(data=data)
+            pathfinder = PathFinder(map)
+            map_image = MapImage(map, pathfinder)
+            map_image.pathfinder.find_optimal_path()
+            print(map_image.canvas)
+
+            output = BytesIO()
+            map_image.draw_image()
+            map_image.draw_path()
+            map_image.canvas.save(output, format='PNG')
+
+            image_data = output.getvalue()
+            image_data_url = 'data:image/png;base64,' + base64.b64encode(
+                image_data).decode("utf-8")
+
+    return render(request, "core/index.html", {
+        "form": form,
+        "image_url": image_data_url
+    })
+    
--- pathfinder/urls.py
+++ pathfinder/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views as core_views
 
 urlpatterns = [
+    path('', core_views.index_view),
     path('admin/', admin.site.urls),
 ]
