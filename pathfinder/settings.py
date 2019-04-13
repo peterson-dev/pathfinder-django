8b696d485339775da0af4559ebb3453a8134987d
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-12 20:52:40 -0400

    configure heroku

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@)m%q8aga4@p&1sn$hjc8bz4jp3zmx2vk+ks(*ev6(ni(^d^do'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
