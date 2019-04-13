45c6cbab6e1e84dfbf3e143550d214615ed914b7
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-12 21:00:23 -0400

    move heroku setting to bottom of django settings

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
